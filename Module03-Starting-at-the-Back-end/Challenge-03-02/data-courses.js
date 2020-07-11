3b7daf0a5d3cc154d4797d6397fc3a25cfc9a03c
Abner Willys <abwillys001@gmail.com>
2020-07-11 01:00:16 -0400

    Add git ignore

--- Module03-Starting-at-the-Back-end/Challenge-03-02/data-courses.js
+++ Module03-Starting-at-the-Back-end/Challenge-03-02/data-courses.js
@@ -1,19 +1,19 @@
 module.exports = [
     {
         id: "starter",
-        image: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        image: "/assets/starter.svg",
         content: "02 Módulos",
         price: "Gratuito"
     },
     {
         id: "launchbase",
-        image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        image: "/assets/launchbase.svg",
         content: "08 Módulos",
         price: "Pago"
     },
     {
         id: "gostack",
-        image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        image: "/assets/gostack.svg",
         content: "10 Módulos",
         price: "Pago"
     }
--- Module03-Starting-at-the-Back-end/Challenge-03-03/data-courses.js
+++ Module03-Starting-at-the-Back-end/Challenge-03-03/data-courses.js
@@ -1,21 +1,21 @@
 module.exports = [
     {
         id: "Starter",
-        image: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        image: "/assets/starter.svg",
         content: "02 Módulos",
         price: "Gratuito",
         course_choose: "Comece já!"
     },
     {
         id: "Launchbase",
-        image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        image: "/assets/launchbase.svg",
         content: "08 Módulos",
         price: "Pago",
         course_choose: "Reserve sua vaga!"
     },
     {
         id: "Gostack",
-        image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        image: "/assets/gostack.svg",
         content: "10 Módulos",
         price: "Pago",
         course_choose: "Reserve sua vaga!"
