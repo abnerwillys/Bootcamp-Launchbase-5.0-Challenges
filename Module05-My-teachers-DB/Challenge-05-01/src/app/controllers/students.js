a02dbeeb35a8ddafd55e5b6ffb2e5b28e61312e9
Abner Willys <abwillys001@gmail.com>
2020-08-20 01:08:48 -0400

    Finishing challenge 05-01

---
+++ Module05-My-teachers-DB/Challenge-05-01/src/app/controllers/students.js
@@ -0,0 +1,39 @@
+const {age, graduation, date, transformExpertise} = require('../../lib/useful')
+
+module.exports = {
+  index(req, res) {
+    return res.render("students/index")
+  },
+  create(req, res) {
+    return res.render("students/create")
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") return res.send('Please, fill all fields!')
+    }
+    
+    let { avatar_url, name, birth, degree, type_class, expertise} = req.body
+
+    return
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  put(req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") return res.send('Please, fill all fields!')
+    }
+
+    return
+  },
+  delete(req, res) {
+    return
+  },
+}
