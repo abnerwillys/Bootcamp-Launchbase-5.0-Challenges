bfd6b90e4001a5620b22c249176b1cbed9e60b7a
Abner Willys <abwillys001@gmail.com>
2020-08-31 00:54:17 -0400

    mini challenge footer done

---
+++ Module06-Mini-challenges/Footer/data-courses.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        id: "Starter",
+        image: "/assets/starter.svg",
+        content: "02 Módulos",
+        price: "Gratuito",
+        course_choose: "Comece já!"
+    },
+    {
+        id: "Launchbase",
+        image: "/assets/launchbase.svg",
+        content: "08 Módulos",
+        price: "Pago",
+        course_choose: "Reserve sua vaga!"
+    },
+    {
+        id: "Gostack",
+        image: "/assets/gostack.svg",
+        content: "10 Módulos",
+        price: "Pago",
+        course_choose: "Reserve sua vaga!"
+    }
+]
+
+// The property "course_choose" is used when the course is chosen.
